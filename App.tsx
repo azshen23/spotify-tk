a9c58375c74e0e0a6ce4a830b15f1fa00e7ee7eb
Andrew Shen <azshen23@gmail.com>
2024-03-14 22:28:13 -0400

    init commit

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { Button } from "react-native";
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
+
+export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "CLIENT_ID",
+      scopes: ["user-read-email", "playlist-modify-public"],
+      // To follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        scheme: "your.app",
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+    }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+  );
+}
