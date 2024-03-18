cf96865f56ffc40bd7f7020d8aeebb608b0a11ff
Andrew Shen <azshen23@gmail.com>
2024-03-18 11:20:44 -0400

    eas changes

--- app/(app)/(tabs)/You/index.tsx
+++ app/(app)/(tabs)/You/index.tsx
@@ -1,14 +1,23 @@
-import { Pressable, Text } from "react-native";
-import ScreenWrapper from "../../../components/ScreenWrapper";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { useSession } from "../../../../ctx";
 
 export default function Tracks() {
   const { signOut } = useSession();
   return (
-    <ScreenWrapper>
-      <Pressable className="" onPress={signOut}>
-        <Text className="text-white">Sign Out</Text>
-      </Pressable>
-    </ScreenWrapper>
+    <View className="flex-1 bg-tertiary items-center ">
+      <SafeAreaView>
+        <View className="flex-1 items-center">
+          <Text className="text-white font-bold">How Quirky Are You?</Text>
+          <TouchableOpacity
+            className="bg-primary rounded-full pl-10 pr-10 pt-5 pb-5"
+            onPress={async () => {
+              signOut();
+            }}
+          >
+            <Text className="text-white font-bold">Sign Out</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
