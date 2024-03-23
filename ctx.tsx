53821ef5237371fb241fb9747d3fd6d97f1ed6f8
Andrew Shen <azshen23@gmail.com>
2024-03-22 21:26:43 -0400

    Update ctx.tsx

--- ctx.tsx
+++ ctx.tsx
@@ -38,7 +38,7 @@ export function SessionProvider(props: React.PropsWithChildren) {
   const [[], setAccessTokenExpiresAt] = useStorageState("accessTokenExpiresAt");
   const redirectUri =
     process.env.NODE_ENV === "production"
-      ? "spotify-tk://auth"
+      ? "spotify-tk://callback"
       : makeRedirectUri({ scheme: "spotify-tk" });
 
   const [request, response, promptAsync] = useAuthRequest(
